13c577c8fb926b99bb37147d15c03bd92ed449ed
Ivan Pramudya Artura <ivanpramudya26@gmail.com>
2023-03-15 22:10:17 +0700

    Menambah file

---
+++ Exercise-01.py
@@ -0,0 +1,16 @@
+print ("=============================")
+print ("Nama            = Ivan Pramudya Artura")
+print ("NIM             = I0322055")
+print ("Tempat tinggal  = Ponorogo, Jawa Timur")
+print ("=============================")
+
+#Deskripsi dan visualisasi tempat tinggal
+print ("Saya lahir dan dibesarkan di salah satu kabupaten di Provinsi Jawa Timur, yaitu Kabupaten Ponorogo. Kabupaten Ponorogo terkenal dengan salah satu keseniannya, yakni kesenian Reog Ponorogo.")
+print ("Saya sendiri bertempat tinggal tepatnya di Jalan DI Panjaitan, Desa Siman, Kecamatan Siman. Desa saya berjarak tidak terlalu jauh dari kota, sehingga akses menuju kota masih terbilang mudah.")
+print ("Di desa saya biasanya setiap seminggu sekali selalu menampilkan kesenian Reog di Balai Desa yang mana merupakan program pemerintah untuk melestarikan dan memperkenalkan kebudayaan.")
+print ("Di daerah sekitar tempat tinggal saya juga masih terdapat banyak sawah karena mayoritas para warga masih bermata pencaharian sebagai petani. Daerah saya bisa dibilang cukup strategis karena dekat dengan pusat-pusat aktivitas, seperti pasar, sekolah mulai dari SD hingga SMA bahkan perguruan tinggi, dan juga mall.")
+print ("Selain itu, di sini juga banyak ditemui angkringan yang selalu ramai oleh anak muda apalagi saat malam Minggu. Pada akhir pekan biasanya akan diadakan CFD atau Car Free Day yang bertempat di Jalan Baru dan tidak terlalu jauh dari rumah saya.")
+print ("Acara tersebut berlangsung dari pukul 6 hingga 10 pagi dan banyak sekali penjual jajanan. Setiap tanggal 1 Muharram, di Ponorogo akan selalu diperingati acara Grebeg Suro dengan beragam kegiatan, seperti Kirab Pusaka, Larungan Telaga Ngebel, Festival Reog Nasional dan Reog Mini, dll.")
+
+#Harapan setelah lulus dari Teknik Industri
+print ("Setelah lulus dari prodi Teknik Industri, saya berharap dapat menggunakan ilmu yang sudah saya peroleh dengan sebaik mungkin untuk kepentingan orang banyak dan bekerja di perusahaan besar seperti Pertamina, Freeport, dan Unilever.")
